71864b99126e79714b33fe66079885299f16d670
Taehyun <ddd2520@naver.com>
2018-03-25 23:42:13 +0900

    add image save and connect train button

--- src/imageProcess.py
+++ src/imageProcess.py
@@ -3,13 +3,20 @@ import numpy as np
 
 #cap = cv2.VideoCapture(0)
 def image_capture():
+
     # Read image
     img = cv2.imread('./../res/image1.png', 1)
-    r = cv2.selectROI(img)
+    fromCenter = False
+
+    r = cv2.selectROI("Select ROI", img, fromCenter)
     while True:
+
         imCrop = img[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            print("end")
+            print("end and save")
+            path = "./../res/modify_img/"
+            # filename = "test" + (1) +".jpg"
+            cv2.imwrite(path + "test1.jpg", imCrop)
             cv2.destroyAllWindows()
             break
 
